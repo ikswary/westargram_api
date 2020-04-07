021554eaa1c9e2cc0178671495a2e393097ce7d0
ikswary <ikswary@naver.com>
2020-04-07 21:16:58 +0900

    add : feature returns specific users comments

--- comment/views.py
+++ comment/views.py
@@ -3,6 +3,7 @@ import json
 from django.http import JsonResponse, HttpResponse
 from django.views import View
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
 
 from .models import Comment
 from user.models import User
@@ -27,14 +28,18 @@ class WriteCommentView(View):
 
 
 class ShowCommentView(View):
-    def get(self, request):
+    def get(self, request, target=None):
         all_comments = []
-        comments = Comment.objects.values()
+        if target is None:
+            comments = Comment.objects.values()
+        elif target is not None:
+            target_user = get_object_or_404(User, user_id=target)
+            comments = Comment.objects.filter(user_id=target_user.id)
         if not len(comments):
             return HttpResponse(status=404)
 
+        print(comments)
         for each in comments:
-            all_comments.append(each['comment'])
-
+            print(each)
+            all_comments.append(each.comment)
         return JsonResponse({'messages': all_comments}, status=200)
-
