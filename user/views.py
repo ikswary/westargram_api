f350840d54980c8b2a80f42b97f32a47617a8845
ikswary <ikswary@naver.com>
2020-04-07 14:23:04 +0900

    add : login feature in User apps RegisterView
    typo changed UserData to User in models

--- user/views.py
+++ user/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.core.exceptions import ValidationError
+from django.http import JsonResponse, HttpResponse
+from django.views import View
+from django.core.validators import validate_email
+
+from .models import User
+
+
+class RegisterView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            this_user = User(
+                user_id=data['user_id'],
+                password=data['password'],
+                email=data['email']
+            )
+        except KeyError:
+            return JsonResponse({'messages': 'ARGUMENT_NOT_PASSED'}, status=400)
+
+        if this_user.user_id == '' or this_user.password == '' or this_user.email == '':
+            return JsonResponse({'messages': 'EMPTY_ARGUMENT_PASSED'}, status=400)
+
+        try:
+            validate_email(User.email)
+        except ValidationError:
+            return JsonResponse({'messages': 'INVALID_EMAIL'})
+
+        if not User.objects.filter(user_id=this_user.user_id).exists():
+            return JsonResponse({'messages': 'DUPLICATED_USER_ID'}, status=400)
+
+        this_user.save()
+        return HttpResponse(status=201)
