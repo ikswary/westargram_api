ffc2133907caed357e3b58443f1c5990568b4183
ikswary <ikswary@naver.com>
2020-04-07 13:45:02 +0900

    add : user.UserData

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class UserData(models.Model):
+    user_id    = models.CharField(max_length=50)
+    password   = models.CharField(max_length=400)
+    email      = models.EmailField(max_length=400)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'users'
